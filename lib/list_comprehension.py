68557f1e16c6bce024d4490cc918e129526f108e
i-prince <molatonny@gmail.com>
2023-10-24 18:27:14 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+# Function to return a list of even elements from a sequence of integers
+def return_evens(sequence):
+    return [num for num in sequence if num % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+# Function to add exclamation marks to the end of sentence strings
+def make_exclamation(sentences):
+    return [sentence + '!' for sentence in sentences]
+
+# Example usage
+even_numbers = return_evens([0, 1, 3, 5, 7, 8, 9])
+print(even_numbers)  # Output: [0, 8]
+
+exclamations = make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+print(exclamations)  # Output: ["Hello!", "I'm doing great!", "Python is fun!"]
